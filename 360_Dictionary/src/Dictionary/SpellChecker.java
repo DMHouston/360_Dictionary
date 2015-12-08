5e13ebb1c1c7cf7e688fcff23fdcbbe502b0aef9
jakercates <jrcates@asu.edu>
2015-12-08 00:26:06 -0700

    Update SpellChecker.java

--- 360_Dictionary/src/Dictionary/SpellChecker.java
+++ 360_Dictionary/src/Dictionary/SpellChecker.java
@@ -5,7 +5,43 @@ public class SpellChecker {
 	private TextFile file;
 	public static void main(String[] args) {
 	
+	ArrayList inputWords = new ArrayList();
+	boolean fileExists = false;
+		 		 
+	try
+	{		
+		while(!fileExists)
+		{
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter file name with extension(.txt): ");
+			File inputFile = new File(input.nextLine());
+			
+			if(inputFile.exists())
+			{
+				fileExists = true;
+					
+				input = new Scanner(inputFile);
+				while(input.hasNext())			//needs to account for whitespace/punctuation.
+				{
+					String word = input.next();
+					word = word.toLowerCase();
+					inputWords.add(word);      //needs to account for duplicates
+				}
+				input. close();
+			}
+			else
+			{
+				System.out.println("Could not find file. Please make sure the file is in the directory and try again.");
+			}
+		}
+	}		
+		catch (Exception e){}
+		
+		Collections.sort(inputWords);   //end result is alphabetical order of words in text. 
+		System.out.println(inputWords);	////Send inputWords to the GUI to decide on adding or ignoring the words
 	}
+		
+		
 	private void runSpellcheck(TextFile file, Dictionary dictionary) {
 		
 	}

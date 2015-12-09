d256bf8b2ab2a7f9b06eba4f5243958c44ddbce3
Aaron <darkestnoon@hotmail.com>
2015-12-08 22:00:40 -0700

    Update SpellChecker.java

--- 360_Dictionary/src/Dictionary/SpellChecker.java
+++ 360_Dictionary/src/Dictionary/SpellChecker.java
@@ -1,48 +1,79 @@
 package Dictionary;
 
-public class SpellChecker {
-	private Dictionary dictionary;
-	private TextFile file;
-	public static void main(String[] args) {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SpellChecker
+{
 	
-	ArrayList inputWords = new ArrayList();
-	boolean fileExists = false;
-		 		 
-	try
-	{		
-		while(!fileExists)
-		{
-			Scanner input = new Scanner(System.in);
-			System.out.print("Enter file name with extension(.txt): ");
-			File inputFile = new File(input.nextLine());
-			
-			if(inputFile.exists())
-			{
-				fileExists = true;
-					
-				input = new Scanner(inputFile);
-				while(input.hasNext())			//needs to account for whitespace/punctuation.
-				{
-					String word = input.next();
-					word = word.toLowerCase();
-					inputWords.add(word);      //needs to account for duplicates
-				}
-				input. close();
-			}
-			else
-			{
-				System.out.println("Could not find file. Please make sure the file is in the directory and try again.");
-			}
-		}
-	}		
-		catch (Exception e){}
+	public static void main(String[] args)
+	{
 		
-		Collections.sort(inputWords);   //end result is alphabetical order of words in text. 
-		System.out.println(inputWords);	////Send inputWords to the GUI to decide on adding or ignoring the words
+		Dictionary dictionary =  null;
+		TextFile file = null;
+	
+		boolean fileExists = false;
+		
+		//Attempt to load the dictionary
+		Scanner input = null;
+		ArrayList<String> dictionaryArray = new ArrayList<String>();
+		
+		try
+		{
+			//Create the scanner to take in input
+			input = new Scanner(new File("Dictionary.txt"));
+			String line = input.next();
+			
+			//Add one line at a time to the dictionary array
+			while (input.hasNext())
+			{
+				dictionaryArray.add(line);
+				line = input.next();
+			}
+			
+			input.close();
+		}
+		catch (Exception e)
+		{
+			System.out.println("The dictionary.txt file cannot be found."
+					+ "\nNo words will be found correct."
+					+ "\nA new dictionary.txt file will be output after "
+					+ "a file is finished being processed.");
+			
+			//Close the file in case of an exception
+			if (input != null)
+				input.close();
+			
+			dictionaryArray = new ArrayList<String>();
+		}
+		
+		dictionary = new Dictionary(dictionaryArray);
+		
+		//Prompt user for the file to input
+		try
+		{	
+			//Keep taking input until a proper file is found
+			while(!fileExists)
+			{
+				input = new Scanner(System.in);
+				System.out.print("Enter file name with extension(.txt): ");
+				file = new TextFile(input.nextLine());
+				
+				fileExists = true;
+			}
+		}	
+		catch (Exception e)
+		{
+			System.out.println("Could not find file. Please make sure the file is in the directory and try again.");
+		}
+		
+		
+		runSpellcheck(file, dictionary);
 	}
 		
-		
-	private void runSpellcheck(TextFile file, Dictionary dictionary) {
+	private static void runSpellcheck(TextFile file, Dictionary dictionary)
+	{
 		
 	}
 }

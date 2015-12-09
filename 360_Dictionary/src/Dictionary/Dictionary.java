6293d58e7146a9a78c030dddbaa8107a2262360e
Aaron <darkestnoon@hotmail.com>
2015-12-08 22:01:19 -0700

    Update Dictionary.java

--- 360_Dictionary/src/Dictionary/Dictionary.java
+++ 360_Dictionary/src/Dictionary/Dictionary.java
@@ -1,26 +1,34 @@
 package Dictionary;
 import java.util.ArrayList;
 
-//Dictonary class handles creating the ArrayLists, searching for a word(boolean), adding a word(void), displaying output(void), toSting(String)
-public class Dictionary {
+//Dictionary class handles creating the ArrayLists, searching for a word(boolean), adding a word(void), displaying output(void), toSting(String)
+public class Dictionary
+{
 	// note for array list, default starting size is 10
-	ArrayList<String> dictionary = new ArrayList<String>();
-	ArrayList<String> wordsAdded = new ArrayList<String>();
-	ArrayList<String> ignoredWords = new ArrayList<String>();
-	public Dictionary(ArrayList<String> wordList) {
+	private ArrayList<String> dictionary = new ArrayList<String>();
+	private ArrayList<String> wordsAdded = new ArrayList<String>();
+	private ArrayList<String> ignoredWords = new ArrayList<String>();
+	
+	public Dictionary(ArrayList<String> wordList)
+	{
+		dictionary = wordList;
 	}
 	
 	//Returns true if the word is found in dictionary or ignoredWords.
-	public boolean searchWord(String word) {
+	public boolean searchWord(String word)
+	{
 		boolean foundInEither = (dictionary.contains(word) || ignoredWords.contains(word));
 		return foundInEither;
 	}
 	
 	//Appends the specified element to the end of the dictionary list, adding to either ignored or added words respectively
-	public void addWord(String word) {
-		if(!(searchWord(word)))							//If this word is not found
+	public void addWord(String word)
+	{
+		//If this word is not found
+		if(!(searchWord(word)))					
 		{
-			dictionary.ensureCapacity(dictionary.size() + 1);	//ensures the capacity is enough to add the next word
+			//ensures the capacity is enough to add the next word
+			dictionary.ensureCapacity(dictionary.size() + 1);
 			dictionary.add(word);
 			wordsAdded.ensureCapacity(wordsAdded.size() + 1);
 			wordsAdded.add(word);
@@ -33,7 +41,8 @@ public class Dictionary {
 	}
 	
 	//Outputs the file to standard out, may need to be changed for GUI
-	public void displayOutput() {
+	public void displayOutput()
+	{
         System.out.println("Words in Dictionary: ");
         System.out.println(dictToString(dictionary));
         
@@ -44,12 +53,15 @@ public class Dictionary {
         System.out.println(dictToString(ignoredWords));
 	}
 
-	//Iterates through an ArrayList returning a String with each word seperated by a new line.
-	private String dictToString(ArrayList<String> thisArrayList){
+	//Iterates through an ArrayList returning a String with each word separated by a new line.
+	private String dictToString(ArrayList<String> thisArrayList)
+	{
 		String returnString = "";
-        for (int index = 0; index < thisArrayList.size(); index ++) {
+		
+        for (int index = 0; index < thisArrayList.size(); index ++)
+        {
             returnString += thisArrayList.get(index);
-            returnString += "\r\n";			//simply using "/n" does not work on every platfrom, can change it if you want
+            returnString += "\r\n";			//Using "/n" does not work on every platform
         }
         return returnString;
 	}
